512c3dec8be16490fb1894661f083e5e12093599
Sre Inukollu <sree@itoolsinfo.com>
2019-03-26 23:33:15 -0500

    Trying to fix something

--- 2/debian-9/rootfs/app-entrypoint.sh
+++ 2/debian-9/rootfs/app-entrypoint.sh
@@ -7,10 +7,10 @@ print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "httpd" ]]; then
     # SPLessons custom code - start    
-    cd /tmp/bitnami/pkg/spldreamfactory    
-      nami unpack dfmodifiedproject
-    cd /
-    rm -rf /tmp/bitnami/pkg/spldreamfactory
+    # cd /tmp/bitnami/pkg/spldreamfactory    
+    #   nami unpack dfmodifiedproject
+    # cd /
+    # rm -rf /tmp/bitnami/pkg/spldreamfactory
     # SPLessons custom code - end
   nami_initialize apache php libphp dreamfactory
   info "Starting dreamfactory... "
